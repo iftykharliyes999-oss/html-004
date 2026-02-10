0b64e9ca7b02d4482cf429f69c9374c4a972f41c
iftykharliyes999-oss <iftykharliyes999@gmail.com>
2026-02-10 10:29:19 +0600

    add

---
+++ javaScript/script.js
@@ -0,0 +1,17 @@
+   
+//         alert("Hello world!")
+
+//         document.write("hello world!")
+
+//         console.log("hellow world")
+//    var cup="tea"
+   
+
+   let a =55;
+   let b =44;
+
+   let x= a+b
+
+   document.write(x);
+   console.log(x);
+   alert("uuuuu") 
